08ad9835936da193b23e1c88e9957dc42de0fbd6
潘安 <wslongchen@vip.qq.com>
2015-07-06 09:44:06 +0800

    SDK
    。。。

--- src/mrpan/android/loveproject/DialogActivity.java
+++ src/mrpan/android/loveproject/DialogActivity.java
@@ -2,26 +2,40 @@ package mrpan.android.loveproject;
 
 import org.json.JSONObject;
 
-import com.tencent.tauth.IUiListener;
-import com.tencent.tauth.Tencent;
-import com.tencent.tauth.UiError;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.tencent.connect.common.Constants;
+import com.tencent.tauth.IUiListener;
+import com.tencent.tauth.Tencent;
+import com.tencent.tauth.UiError;
 
 public class DialogActivity extends Activity {
 
 	Tencent mTencent;
 	private final static String APP_ID = "1104753484";
 	private final static String SCOPE = "all";
-	BaseUiListener listener;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dialog_main);
 		mTencent = Tencent.createInstance(APP_ID, this.getApplicationContext());
+		Button next_btn=(Button)findViewById(R.id.next_btn);
+		next_btn.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				login();
+			}
+			
+		});
 	}
 	
 	public void login()
@@ -29,12 +43,13 @@ public class DialogActivity extends Activity {
 		//mTencent = Tencent.createInstance(APP_ID, this.getApplicationContext());
 		if (!mTencent.isSessionValid())
 		{
-			mTencent.login(this, SCOPE, listener);
+			mTencent.login(this, SCOPE, new BaseUiListener());
 	}
 	}
 
 	class BaseUiListener implements IUiListener {
 		protected void doComplete(JSONObject values) {
+			Log.d("JSON", values.toString());
 		}
 
 		@Override
@@ -52,12 +67,18 @@ public class DialogActivity extends Activity {
 		public void onComplete(Object arg0) {
 			// TODO Auto-generated method stub
 			doComplete((JSONObject) arg0);
-			System.out.println(arg0.toString());
+			
 		}
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		mTencent.onActivityResult(requestCode, resultCode, data);
+		if(requestCode == Constants.REQUEST_API) {
+			if(resultCode == Constants.RESULT_LOGIN) {
+			    mTencent.handleLoginData(data, new BaseUiListener());
+		    }
+		    super.onActivityResult(requestCode, resultCode, data);
+		
+		}
 	}
 }
